045fb9447353acd69ed1c1c03aa1068e7140fca8
DanilDubowitsky <48087527+DanilDubowitsky@users.noreply.github.com>
2019-03-28 14:21:12 +0700

    eojfsulg
    sdfds

--- MQ1/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ MQ1/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -76,17 +76,17 @@ public final class R {
         public static final int bottom = 0x7f080034;
         public static final int end = 0x7f08005d;
         public static final int gone = 0x7f08006c;
-        public static final int invisible = 0x7f08007a;
-        public static final int left = 0x7f08007f;
-        public static final int packed = 0x7f08009f;
-        public static final int parent = 0x7f0800a1;
-        public static final int percent = 0x7f0800a4;
-        public static final int right = 0x7f0800ac;
-        public static final int spread = 0x7f0800cf;
-        public static final int spread_inside = 0x7f0800d0;
-        public static final int start = 0x7f0800d5;
-        public static final int top = 0x7f0800f1;
-        public static final int wrap = 0x7f080101;
+        public static final int invisible = 0x7f08007b;
+        public static final int left = 0x7f080080;
+        public static final int packed = 0x7f0800a0;
+        public static final int parent = 0x7f0800a2;
+        public static final int percent = 0x7f0800a5;
+        public static final int right = 0x7f0800ad;
+        public static final int spread = 0x7f0800d0;
+        public static final int spread_inside = 0x7f0800d1;
+        public static final int start = 0x7f0800d6;
+        public static final int top = 0x7f0800f2;
+        public static final int wrap = 0x7f080102;
     }
     public static final class styleable {
         private styleable() {}
--- MQ1/app/src/main/java/com/example/mq1/Fragment/HomeFragment.java
+++ MQ1/app/src/main/java/com/example/mq1/Fragment/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.mq1.Fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -9,24 +10,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.mq1.MainActivity;
 import com.example.mq1.MainLenta;
 import com.example.mq1.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
     private Button logoutB;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        logoutB = (Button) container.findViewById(R.id.LogOutB);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        logoutB = (Button) view.findViewById(R.id.LogOutB);
+        logoutB.setOnClickListener(this);
 //        logoutB.setOnClickListener(this);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 
 
     @Override
     public void onClick(View view) {
-
+        if(view == logoutB){
+            firebaseAuth.signOut();
+            startActivity(new Intent(getActivity(), MainActivity.class));
+        }
     }
 }
